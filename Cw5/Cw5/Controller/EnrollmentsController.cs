22519370efdc4d16b7f601fba5e6489263e4d759
Emilia Cholewicka s18914 <s18914@pjwstk.edu.pl>
2020-05-03 13:27:49 +0200

    add tokens cwiczenia7

--- Cw5/Cw5/Controller/EnrollmentsController.cs
+++ Cw5/Cw5/Controller/EnrollmentsController.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Cw5.DTOs.Requests;
 using Cw5.DTOs.Responses;
 using CW5.DTOs.Request;
 using CW5.DTOs.Response;
 using CW5.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Cw5.Controller
 {
@@ -16,10 +23,13 @@ namespace Cw5.Controller
         private const String ConString = "Data Source=db-mssql;Initial Catalog=s18914;Integrated Security=True";
 
         private readonly IStudentsDbService _dbService;
+        private IConfiguration _configuration;
 
-        public EnrollmentsController(IStudentsDbService dbService)
+
+        public EnrollmentsController(IStudentsDbService dbService, IConfiguration configuration)
         {
             _dbService = dbService;
+            _configuration = configuration;
         }
 
 
@@ -43,6 +53,38 @@ namespace Cw5.Controller
 
             pres = _dbService.PromoteStudent(preq);
             return Created("promote", pres);
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+
+            //sprawdzam login i haslo
+
+            var claims = new[]
+           {
+                new Claim(ClaimTypes.Name, "Mila123"),
+                new Claim(ClaimTypes.Role, "employee")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "https://localhost",
+                audience: "https://localhost",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: creds
+                );
+
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                refreshToken = "token"
+            });
+
         }
 
 
